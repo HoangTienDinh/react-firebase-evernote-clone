c6cc1a6df2640cd6fbd563f9f49fdca47580a5b4
Hoang Dinh <hoangdinh90@gmail.com>
2020-07-28 13:52:25 -0700

    connect firebase to update new notes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const firebase = require("firebase");
 
 class App extends React.Component {
-
-
-
-  render() {
-
-    return (
-      <div>Hello World</div>
-    )
+  constructor() {
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null,
+    };
   }
 
+  render() {
+    return <div>Hello World</div>;
+  }
 
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection("notes")
+      .onSnapshot((serverUpdate) => {
+        const notes = serverUpdate.docs.map((doc) => {
+          const data = doc.data();
+          data["id"] = doc.id;
+          return data;
+        });
+        console.log(notes)
+        this.setState({ notes: notes });
+      });
+  };
 }
 
 export default App;
